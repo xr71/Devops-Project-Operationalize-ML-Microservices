2e08537e37357c3458da65c71ab91de324958d08
Xu Ren <musicalxu@hotmail.com>
2020-04-05 23:42:53 +0800

    complete kubernetes section and files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,23 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+# should be same as what was pushed in upload_docker.sh
+
+dockerpath=xuren71/my-ml-microservice
+
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment mlkluster --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward $(kubectl get pods -o=name | grep mlkluster) 8000:80
 
